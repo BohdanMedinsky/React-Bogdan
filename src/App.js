63f4df350654d9ca307a4ec8696476cdee4739d5
BohdanMedinsky <mbogdan4ik2001@gmail.com>
2023-02-06 18:57:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import BookDisplay from './my-app/BookDisplay';
+import Music from './my-app/Music';
+import Cooks from './my-app/Cooks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+      <div className="App">
+        <BookDisplay />
+      </div>
+      <div className="App">
+        <Music />
+      </div>
+      <div className="App">
+        <Cooks />
+      </div>
+      
+      </>
+    );
+  }
 }
+
+
 
 export default App;
---
+++ src/my-app/BookDisplay.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './BookDisplay.css';
+ 
+
+const profile = {
+  date: 'Рік видання 30 липня 2016 р ',
+  name: 'Гаррі Потер і прокляте диття',
+  pages: 'Кількість сторінок 302',
+  genre: 'Жанр фентезі ', 
+  garri:' Про книгу : Гаррі Поттер і прокляте дитя» — пєса в двох частинах британської письменниці Джоан Роулінг; написана в співавторстві зі сценаристом Джеком Торном і режисером Джоном Тіффані.',
+  author: 'Автор Джуан Роулінг', 
+  src: 'https://upload.wikimedia.org/wikipedia/uk/f/fe/HP_and_the_Cursed_Child_UKR.png',
+}; 
+                        
+class BookDisplay extends Component {
+  render() {
+    return (
+      <>
+
+      <div class="card">
+ <img src={profile.src} alt=''></img>
+  <div class="container">
+  <h2>{profile.name}</h2>
+  <p>{profile.date}</p>
+  <p>{profile.pages}</p>
+  <p>{profile.author}</p>
+  <p>{profile.genre}</p>
+  <p>{profile.garri}</p>
+  </div>
+</div>
+
+
+      <hr></hr>
+      </>
+      );
+    }
+}
+
+export default BookDisplay;
